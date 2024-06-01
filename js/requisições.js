bd718292ea82d26060aa06ea500c70e944bfa74a
jose-rf <joserfranca13@gmail.com>
2024-06-01 16:51:12 -0300

    Correção de problemas e iplementação de função para adicionar produtos  no db

--- js/requisições.js
+++ js/requisições.js
@@ -5,6 +5,39 @@ export async function listaDeProdutos() {
     return conexaoConvertida;
 }
 
+async function criaProduto(nome, preco, imagem){
+
+       // Função para formatar o preço
+       function formatarPreco(preco) {
+        if (Number.isInteger(preco)) {
+            // Se for inteiro, adiciona ".00"
+            return preco.toFixed(2);
+        }
+        // Se for decimal, retorna o preço como string
+        return preco.toString();
+    }
+
+    const precoFormatado = formatarPreco(preco);
+
+    const conexao = await fetch("http://localhost:3000/produtos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            nome : nome,
+            pereco : `R$${preco}`,
+            imagem : imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+
+}
+
 export const conectaApi = {
-    listaDeProdutos
-};
+    listaDeProdutos,
+    criaProduto
+}
+
+
